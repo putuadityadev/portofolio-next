d421395599f508d8d27f9f51a723ce3056aaf6fb
aditya <adityamph1@gmail.com>
2025-01-01 14:50:36 +0800

    /portofolio-next

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-[100%] mx-auto",
     titleClassName: "justify-end",
-    img: "/profile_img.png",
+    img: "/portofolio-next/profile_img.png",
     spareImg: "",
   },
   {
@@ -43,7 +43,7 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
+    img: "/portofolio-next/grid.svg",
     spareImg: "/b4.svg",
   },
 
@@ -54,7 +54,7 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
+    img: "/portofolio-next/b5.svg",
     spareImg: "/grid.svg",
   },
   {
@@ -74,7 +74,7 @@ export const projects = [
     id: 1,
     title: "Nike Shoes Landing Page",
     des: "Recreate Nike official landing page with React.js and TailwindCSS for Learning purpose.",
-    img: "/nike-clone.png",
+    img: "/portofolio-next/nike-clone.png",
     iconLists: ["/re.svg", "/tail.svg",],
     link: "https://putuadityadev.github.io/nike-clone/",
   },
@@ -82,7 +82,7 @@ export const projects = [
     id: 2,
     title: "Amazon - Clone Ecommerce Website",
     des: "An ecommerce website inspire by Amazon with Full Javascript and Pure CSS Technologies",
-    img: "/amazon-clone.png",
+    img: "/portofolio-next/amazon-clone.png",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
     link: "https://putuadityadev.github.io/amazon-clone-app/",
   },
@@ -90,7 +90,7 @@ export const projects = [
     id: 3,
     title: "EpiCenter - Fullstack Ecommerce-Website",
     des: "A REAL Ecommerce website with auth, and payment integration. Build with Djanggo Python and Bootstrap.",
-    img: "/epicenter.png",
+    img: "/portofolio-next/epicenter.png",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
     link: "https://github.com/putuadityadev/django-final",
   },
@@ -98,7 +98,7 @@ export const projects = [
     id: 4,
     title: "Animated Apple Iphone 3D Website",
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
+    img: "/portofolio-next/p4.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
     link: "https://putuadityadev.github.io/apple-website/",
   },
@@ -131,31 +131,31 @@ export const companies = [
   {
     id: 1,
     name: "cloudinary",
-    img: "/cloud.svg",
+    img: "/portofolio-next/cloud.svg",
     nameImg: "/cloudName.svg",
   },
   {
     id: 2,
     name: "appwrite",
-    img: "/app.svg",
+    img: "/portofolio-next/app.svg",
     nameImg: "/appName.svg",
   },
   {
     id: 3,
     name: "HOSTINGER",
-    img: "/host.svg",
+    img: "/portofolio-next/host.svg",
     nameImg: "/hostName.svg",
   },
   {
     id: 4,
     name: "stream",
-    img: "/s.svg",
+    img: "/portofolio-next/s.svg",
     nameImg: "/streamName.svg",
   },
   {
     id: 5,
     name: "docker.",
-    img: "/dock.svg",
+    img: "/portofolio-next/dock.svg",
     nameImg: "/dockerName.svg",
   },
 ];
@@ -166,45 +166,45 @@ export const workExperience = [
     title: "Computer Sience Student",
     desc: "A CS student in Primakara University major Information System.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "/portofolio-next/exp1.svg",
   },
   {
     id: 2,
     title: "Self Developers",
     desc: "Designed and developed website for my purpose to learning and startup.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    className: "md:col-span-2", 
+    thumbnail: "/portofolio-next/exp2.svg",
   },
   {
     id: 3,
     title: "Freelance Web Dev Project",
     desc: "Led the dev of a website app for a client, from initial concept to deployment on site.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    className: "md:col-span-2",
+    thumbnail: "/portofolio-next/exp3.svg",
   },
   {
     id: 4,
     title: "Content Creator",
     desc: "I'm currently a content creator making videos about programing.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "/portofolio-next/exp4.svg",
   },
 ];
 
 export const socialMedia = [
   {
     id: '1',
-    img: "/git.svg",
+    img: "/portofolio-next/git.svg",
     link: "https://github.com/putuadityadev"
   },
   {
     id: '2',
-    img: "/insta.svg",
+    img: "/portofolio-next/insta.svg",
     link: "https://www.instagram.com/tu.aditya_/"
   },
   {
     id: '3',
-    img: "/link.svg",
+    img: "/portofolio-next/link.svg",
     link: "https://linkedin.com/in/putuadit"
   },
 ];
